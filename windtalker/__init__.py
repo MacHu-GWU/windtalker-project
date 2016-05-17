5f6bbf9e2e310586b9943454a2363978a661d3d7
MacHu-GWU <husanhe@gmail.com>
2016-05-17 05:10:23 -0400

    2016-05-17
    update the short description

--- windtalker/__init__.py
+++ windtalker/__init__.py
@@ -2,4 +2,4 @@
 # -*- coding: utf-8 -*-
 
 __version__ = "0.0.4"
-__short_description__ = "Super easy to use encryption and decryption tool"
+__short_description__ = "Super easy-to-use encryption and decryption tool"
